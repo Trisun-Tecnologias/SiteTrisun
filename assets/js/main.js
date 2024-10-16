6ea87caadd7a8e2f4573f8bda9ee07bb5df832b5
NNetoni <nilsoncarvalho11@gmail.com>
2024-10-16 17:28:26 -0300

    Porfolio section update(now working)

--- assets/js/main.js
+++ assets/js/main.js
@@ -175,6 +175,7 @@
     }
     mailChimp();
 
+    
 
 
 // 12 Pop Up Img
@@ -188,7 +189,27 @@
         });
       }
 
+  // Porfolio isotope and filter
+  $(window).on('load', function() {
+    var portfolioIsotope = $('.portfolio-container').isotope({
+      itemSelector: '.portfolio-item',
+      layoutMode: 'fitRows'
+    });
 
+    $('#portfolio-flters li').on('click', function() {
+      $("#portfolio-flters li").removeClass('filter-active');
+      $(this).addClass('filter-active');
+
+      portfolioIsotope.isotope({
+        filter: $(this).data('filter')
+      });
+    });
+
+    // Initiate venobox (lightbox feature used in portofilo)
+    $(document).ready(function() {
+      $('.venobox').venobox();
+    });
+  });
 
 
 })(jQuery);
